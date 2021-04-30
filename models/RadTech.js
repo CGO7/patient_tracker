2eae0f87afbba18e438779f4950a09c950f28083
Leticia-Mendiola <lmendiola509@gmail.com>
2021-04-30 18:10:16 -0500

    created RadTech.js

---
+++ models/RadTech.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Surgeon extends Model {}
+
+Surgeon.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    phone_number: {
+      type: DataTypes.STRING(15),
+      allowNull: false,
+    },
+    surgery_id: {
+      references: 'service',
+      key: 'id',
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'doctor',
+  }
+);
+
+module.exports = Surgeon;
