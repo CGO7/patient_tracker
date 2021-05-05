81a575e04ae83d620ee747fdd218d48c5ac4b74f
Leticia-Mendiola <lmendiola509@gmail.com>
2021-05-04 22:09:15 -0500

    added bulk create methods to seeds/index.js

--- seeds/index.js
+++ seeds/index.js
@@ -7,16 +7,21 @@ const { Patient, Personnel, Room } = require('../models');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
+
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+  await Posts.bulkCreate(posts, {
+    individualHooks: true,
+    returning: true,
+  });
+  await Comments.bulkCreate(comments, {
+    individualHooks: true,
+    returning: true,
+  });
+
   console.log('\n----- DATABASE SYNCED -----\n');
-
-  await seedRoom();
-  console.log('\n----- ROOM SEEDED -----\n');
-
-  await seedPersonnel();
-  console.log('\n----- PERSONNEL SEEDED -----\n');
-
-  await seedPatient();
-  console.log('\n----- PATIENT SEEDED -----\n');
 
   process.exit(0);
 };
