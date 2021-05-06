23d94fe3a09655164e7f7ae178dddd8fabf035bd
John Hinojosa <hinojohn@ymail.com>
2021-05-05 19:17:54 -0500

    added util folder, and auth (helper), removed some dependencies

--- controllers/apis/index.js
+++ controllers/apis/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const personnel_routes = require('./personnel_routes.js');
-const patient_routes = require('./patient-routes.js');
+const patient_routes = require('./patient_routes.js');
 const room_routes = require('./room_routes.js');
 
 
