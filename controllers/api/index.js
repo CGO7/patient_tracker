d4f5610698e950fe4c9258ed115e220f96690803
John Hinojosa <hinojohn@ymail.com>
2021-05-10 14:01:09 -0500

    added service routes to route index

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,10 +3,12 @@ const personnel_routes = require('./personnel_routes.js');
 const patient_routes = require('./patient_routes.js');
 const room_routes = require('./room_routes.js');
 const user_routes = require('./user_routes.js');
+const service_routes = require('./service_routes.js');
 
 router.use('/personnel', personnel_routes);
 router.use('/patients', patient_routes);
 router.use('/rooms', room_routes);
 router.use('/users', user_routes);
+router.use('/services', service_routes);
 
 module.exports = router;
